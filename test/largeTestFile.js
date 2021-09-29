3cc05d8db881249494d3839301aefeecd9c1ec42
Sam Rogers <samrogers15@gmail.com>
2021-09-29 09:27:51 -0700

    Added additional file to handle large JSON test

---
+++ test/largeTestFile.js
@@ -0,0 +1,6 @@
+const { deduplicateLeads } = require("../src/Functions/deduplicateLeads");
+const { output } = require("../src/Functions/outputRevisedLeads");
+const leads = require("../src/Inputs/largeLeads.json").leads;
+const revisedLeads = "../src/Outputs/testOutput.json";
+
+output(revisedLeads, deduplicateLeads(leads));
